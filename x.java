d209812f81d223435e824f1b389ad2081911b9be
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-07-09 09:16:38 +0530

    Add files via upload

---
+++ x.java
@@ -0,0 +1,20 @@
+package collecton;
+
+import java.util.HashMap;
+
+public class x {
+	public static void main(String[] args) {
+		HashMap<Integer,String> hm = new HashMap<>();
+		hm.put(1, "abc");
+		hm.put(2, "chd");
+		hm.put(1, "nsk");
+		hm.put(3, "mkd");
+		hm.put(4, "abc");
+		System.out.println(hm);
+		
+	}
+	
+		
+		
+
+}
